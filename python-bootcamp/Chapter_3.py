aaad358eaa00e33aab5207763fd67cc13b665761
ridhimapandey117 <ridhimap711@gmail.com>
2026-06-26 15:02:52 -0400

    Python Bootcamp Udemy Section 3 Practice Problems

---
+++ python-bootcamp/Chapter_3.py
@@ -0,0 +1,17 @@
+#Simple Arthemetic
+print(49+51)
+
+#Printing
+# Lines that start with hashtags are comments
+# Write your code below that prints out "Hello World"
+# Make sure your spacing and capitalization matches.
+print("Hello World")
+
+#String indexing
+# Write your string index below
+# Start with 'Hello World'
+# and make sure to match spaces and capitalization exactly
+print("Hello World"[8])
+
+#String Splicing
+print('tinker'[1:4])
